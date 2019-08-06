6d2a3efdc52c9a121f7e10555f8371e6a4b1ac2d
Igor Artemov (MBP) <i_artemov@omobus.net>
2019-08-06 13:47:06 +0300

    data: added [country_ids] attribute to the [quest_rows] stream.

--- omobus-zero-db.sql
+++ omobus-zero-db.sql
@@ -573,6 +573,7 @@ create table quest_rows (
     descr 		descr_t 	not null,
     qtype 		varchar(7) 	null check(ftype=0 and qtype in ('boolean','integer') or (ftype<>0 and qtype is null)),
     extra_info 		note_t 		null,
+    country_ids 	countries_t 	null,
     row_no 		int32_t 	null, -- ordering
     primary key(qname_id, qrow_id)
 );
