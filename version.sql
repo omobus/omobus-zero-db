83ad49c572cc6783c1338a056183b3abb3cc02d9
Igor Artemov (MBP) <i_artemov@omobus.net>
2019-09-07 18:38:25 +0300

    3.4.22 release

--- version.sql
+++ version.sql
@@ -1,5 +1,5 @@
 /* Copyright (c) 2006 - 2019 omobus-zero-db authors, see the included COPYRIGHT file. */
 
-update sysparams set param_value='3.4.21' where param_id='db:vstamp';
+update sysparams set param_value='3.4.22' where param_id='db:vstamp';
 
 go
