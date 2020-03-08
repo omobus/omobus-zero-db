d8dfacfa164441d1a445f0106d64a3f9b0870ea1
Sergey Gomongulov <support@omobus.net>
2020-03-08 17:07:05 +0300

    data: added [cookie] attribute to the [agreements1] stream.

--- omobus-zero-db.sql
+++ omobus-zero-db.sql
@@ -130,6 +130,7 @@ create table agreements1 (
     b_date 		date_t 		not null,
     e_date 		date_t 		not null,
     strict 		bool_t 		not null default 0,
+    cookie 		uid_t 		null,
     primary key (account_id, placement_id, posm_id, b_date)
 );
 
