5dedc51628b934bf2682bced2f10f7f4a911b825
Igor Artemov <support@omobus.net>
2020-08-04 13:51:50 +0300

    3.5.2 release

--- version.sql
+++ version.sql
@@ -1,5 +1,5 @@
 /* Copyright (c) 2006 - 2020 omobus-zero-db authors, see the included COPYRIGHT file. */
 
-update sysparams set param_value='3.5.1' where param_id='db:vstamp';
+update sysparams set param_value='3.5.2' where param_id='db:vstamp';
 
 go
