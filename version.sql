25a1691d2e001a76588d633cfc97c4b01fb94e48
Igor Artemov <support@omobus.net>
2022-06-19 10:15:02 +0300

    3.5.21 release

--- version.sql
+++ version.sql
@@ -1,5 +1,5 @@
 /* Copyright (c) 2006 - 2022 omobus-zero-db authors, see the included COPYRIGHT file. */
 
-update sysparams set param_value='3.5.20' where param_id='db:vstamp';
+update sysparams set param_value='3.5.21' where param_id='db:vstamp';
 
 go
