3bdc59c067e91b40f70e83e248d2651a77db06d7
Igor Artemov <support@omobus.net>
2023-01-14 17:22:30 +0300

    data: changed [account_id] attribute with [rc_id] at the [plu_codes] stream.

--- omobus-zero-db.sql
+++ omobus-zero-db.sql
@@ -646,7 +646,7 @@ create table placements (
 );
 
 create table plu_codes (
-    account_id 		uid_t 		not null,
+    rc_id 		uid_t 		not null,
     prod_id 		uid_t 		not null,
     plu_code 		code_t 		not null,
     primary key(account_id, prod_id)
